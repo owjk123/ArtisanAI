efef8d73971ec973bd51e336dd405892c4063328
owjk123 <owjk123@github.com>
2026-04-05 13:28:00 +0800

    fix: BasicTextField import

--- app/src/main/java/com/artisanai/ui/components/Components.kt
+++ app/src/main/java/com/artisanai/ui/components/Components.kt
@@ -4,6 +4,7 @@ package com.artisanai.ui.components
 import androidx.compose.animation.*
 import androidx.compose.animation.core.*
 import androidx.compose.foundation.*
+import androidx.compose.foundation.text.BasicTextField
 import androidx.compose.foundation.layout.*
 import androidx.compose.foundation.shape.RoundedCornerShape
 import androidx.compose.material3.*

6da2f89ab4f834f911cd90b73638961c57eb75f6
owjk123 <160257207+owjk123@users.noreply.github.com>
2026-04-05 12:14:27 +0800

    Add app/src/main/java/com/artisanai/MainActivity.kt

---
+++ app/src/main/java/com/artisanai/MainActivity.kt
@@ -0,0 +1,42 @@
+package com.artisanai
+
+import android.os.Bundle
+import androidx.activity.ComponentActivity
+import androidx.activity.compose.setContent
+import androidx.activity.enableEdgeToEdge
+import androidx.activity.viewModels
+import androidx.compose.foundation.layout.fillMaxSize
+import androidx.compose.material3.windowsizeclass.ExperimentalMaterial3WindowSizeClassApi
+import androidx.compose.material3.windowsizeclass.calculateWindowSizeClass
+import androidx.compose.runtime.collectAsState
+import androidx.compose.runtime.getValue
+import androidx.compose.ui.Modifier
+import com.artisanai.ui.screens.MainScreen
+import com.artisanai.ui.theme.ArtisanTheme
+import com.artisanai.viewmodel.MainViewModel
+
+@OptIn(ExperimentalMaterial3WindowSizeClassApi::class)
+class MainActivity : ComponentActivity() {
+
+    private val viewModel: MainViewModel by viewModels {
+        MainViewModel.Factory(this)
+    }
+
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+        enableEdgeToEdge()
+
+        setContent {
+            ArtisanTheme {
+                val uiState by viewModel.uiState.collectAsState()
+                val windowSizeClass = calculateWindowSizeClass(this)
+
+                MainScreen(
+                    uiState = uiState,
+                    viewModel = viewModel,
+                    windowSizeClass = windowSizeClass
+                )
+            }
+        }
+    }
+}
